7af39006aa6eae1e9271f9e405f586345c6ca685
Alexandre Estrela <82901702+alexandre11aa@users.noreply.github.com>
2024-07-24 18:41:49 -0300

    Update movies_carousel.js

--- frontend/js/movies_carousel.js
+++ frontend/js/movies_carousel.js
@@ -1,17 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const movieBar = document.querySelector('.movie-bar');
-    const movieItemsContainer = document.querySelector('.movie-items-container');
-
-    // Defina o tempo de rotação (em milissegundos)
-    const scrollDuration = 120000; // 40 segundos
-
-    movieItemsContainer.style.animation = `scroll ${scrollDuration}ms linear infinite`;
-
-    movieBar.addEventListener('mouseenter', () => {
-        movieItemsContainer.style.animationPlayState = 'paused';
-    });
-
-    movieBar.addEventListener('mouseleave', () => {
-        movieItemsContainer.style.animationPlayState = 'running';
+$(document).ready(function(){
+    $('.slick-carousel').slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: true,
+        arrows: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 5,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
